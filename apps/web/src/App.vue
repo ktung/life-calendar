23f252e1565b0bd9cc65593003f70edaad500397
ktung <ptung@protonmail.ch>
2023-09-07 21:45:37 -0400

    feat: add header

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -5,6 +5,9 @@ import LifeCalendar from './components/LifeCalendar.vue';
 </script>
 
 <template>
+  <header>
+    Life Calendar
+  </header>
   <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
